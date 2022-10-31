f4973660f0223104a51725aaefbd13d852be019b
crxtzonceu <zamanthaquijano@gmail.com>
2022-11-01 01:33:33 +0800

    "Done"

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let rep = ""
+  for(let i = 0;i < num;i++){
+    rep += string
+  }
+  if(num < 0){
+    return "ERROR"
+  } else {
+    return rep
+  }
 };
 
 // Do not edit below this line
