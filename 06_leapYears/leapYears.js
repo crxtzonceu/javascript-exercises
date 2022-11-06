9c9014a46e2a62d0897a9d1a3b7eecc43e893b4a
crxtzonceu <zamanthaquijano@gmail.com>
2022-11-06 20:13:15 +0800

    done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,11 @@
-const leapYears = function() {
+const leapYears = yr => {
+  if(yr % 4 == 0 && (yr % 100 != 0 || yr % 400 == 0)){
+    return true
+  } else {
+    return false
+  }
+}
 
-};
+// this top answer is confusing but finally i understood it
 
-// Do not edit below this line
 module.exports = leapYears;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(fahr) {
+  return +((fahr - 32) * 5.0 / 9.0).toFixed(1)
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  return +(celc * (9.0 / 5.0) + 32).toFixed(1)
 };
 
 // Do not edit below this line
